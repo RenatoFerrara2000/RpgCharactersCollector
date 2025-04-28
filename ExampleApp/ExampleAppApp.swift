854fe4f1400794425808c7d2c15f807eb06f20f7
renato Ferrara <reny.2000@hotmail.it>
2025-04-28 11:23:31 +0200

    Initial Commit

---
+++ ExampleApp/ExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleAppApp.swift
+//  ExampleApp
+//
+//  Created by Renato Ferrara on 28/04/25.
+//
+
+import SwiftUI
+
+@main
+struct ExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
